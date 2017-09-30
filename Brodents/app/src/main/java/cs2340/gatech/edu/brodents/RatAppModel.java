83d436c21aaf5d29288cb3dc66d6674b3c7f1145
bentemplin <bentemplin@users.noreply.github.com>
2017-09-30 11:31:14 -0400

    Implemented registerUsers method.
    Users can now be registered through the RatAppModel class. Also made
    everything in RatAppModel package-private

--- Brodents/app/src/main/java/cs2340/gatech/edu/brodents/RatAppModel.java
+++ Brodents/app/src/main/java/cs2340/gatech/edu/brodents/RatAppModel.java
@@ -2,6 +2,7 @@ package cs2340.gatech.edu.brodents;
 
 import android.util.Log;
 
+import java.security.SecureRandom;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -14,7 +15,7 @@ import java.sql.SQLException;
  * @version 1.0
  */
 
-public class RatAppModel {
+class RatAppModel {
     private DatabaseConnector db;
     private boolean dbInitialized;
     private static RatAppModel model;
@@ -32,7 +33,7 @@ public class RatAppModel {
     /**
      * This method initializes the model and tries to create a database connection.
      */
-    public static void initialize() {
+    static void initialize() {
         model = new RatAppModel("ratapp", "2Z2MqYE!cLgNJu8R",
                 "104.236.213.171:3306/rats");
         Log.d("RatAppModel", "Initialized");
@@ -42,13 +43,13 @@ public class RatAppModel {
      * This method gets the current RatAppModel.
      * @return RatAppModel currently being maintained.
      */
-    public static RatAppModel getInstance() {return model;}
+    static RatAppModel getInstance() {return model;}
 
     /**
      * This method returns whether the Database connection has been initialized.
      * @return Boolean whether the Database connection has been initialized
      */
-    public boolean isDbInitialized() {return dbInitialized;}
+    boolean isDbInitialized() {return dbInitialized;}
 
     /**
      * This method tests whether the passed in username and password are valid credentials
@@ -56,7 +57,7 @@ public class RatAppModel {
      * @param password The password (in plaintext) to test
      * @return boolean of the result of the test
      */
-    public boolean testCredentials(String userName, String password) {
+    boolean testCredentials(String userName, String password) {
         String getUsersText = "SELECT userName, password, salt FROM users WHERE username=?";
         ResultSet results;
         try {
@@ -84,5 +85,52 @@ public class RatAppModel {
             //e.printStackTrace();
             return false;
         }
+    }
+
+    /**
+     * This method registers a user in the database. After registration they will instantly be able
+     * to log in to the system.
+     *
+     * NOTE: this method does not do any input validation. You should check
+     * to make sure usernames, passwords, profile names, and home locations match your specific
+     * criteria before calling this method.
+     * @param userName The username under which to register the user
+     * @param password The password to use for the user's account
+     * @param profileName The profile name to display for the user
+     * @param homeLocation The user's home location
+     * @return An integer code indicating the success of the registration. 0 if registration
+     *         succeeds, 1 if the registration fails because the username is already taken, and
+     *         2 if a SQLException occurs during registration;
+     */
+    int registerUser(String userName, String password, String profileName,
+        String homeLocation) {
+        SecureRandom saltShaker = new SecureRandom();
+        try {
+            PreparedStatement checkStatment = db.getStatement("SELECT * FROM users WHERE userName"
+            + "=?");
+            checkStatment.setString(1, userName);
+            ResultSet checkResults = db.query(checkStatment);
+            if (checkResults.next()) {
+                return 1;
+            } else {
+                int salt = saltShaker.nextInt(32);
+                String hashedPass = PasswordHasher.getSecurePassword(Integer.toHexString(salt),
+                        password);
+                String registrationText = "INSERT INTO users(userName, password, profileName, "
+                        + "homeLocation, salt) VALUES(?, ?, ?, ?, ?)";
+                PreparedStatement registerStatement = db.getStatement(registrationText);
+                registerStatement.setString(1, userName);
+                registerStatement.setString(2, hashedPass);
+                registerStatement.setString(3, profileName);
+                registerStatement.setString(4, homeLocation);
+                registerStatement.setInt(5, salt);
+                db.update(registerStatement);
+                return 0;
+            }
+        } catch (SQLException e) {
+            Log.e("Register User", e.getMessage());
+            return 2;
+
+        } 
     }
 }
