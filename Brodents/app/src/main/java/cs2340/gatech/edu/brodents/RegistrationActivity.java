a6fb5a6bb414dacd31e4f34bb5aea760b4eee3eb
Rikesh Subedi <rsubedi3@gatech.edu>
2017-10-02 13:43:19 -0400

    Make registration page functional.

--- Brodents/app/src/main/java/cs2340/gatech/edu/brodents/RegistrationActivity.java
+++ Brodents/app/src/main/java/cs2340/gatech/edu/brodents/RegistrationActivity.java
@@ -1,13 +1,29 @@
 package cs2340.gatech.edu.brodents;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
+/**
+ * Class controller to manage registration for Brodents.
+ * @author Rikesh Subedi
+ * @version 1.0
+ */
 public class RegistrationActivity extends AppCompatActivity {
+    private UserRegistrationTask mAuthTask = null;
+    private EditText mUser;
+    private EditText mPassword;
+    private EditText mName;
+    private EditText mHome;
+    private CheckBox mAdmin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +32,167 @@ public class RegistrationActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        mUser = (EditText) findViewById(R.id.txtUsername);
+        mPassword = (EditText) findViewById(R.id.txtPassword);
+        mName = (EditText) findViewById(R.id.txtName);
+        mHome = (EditText) findViewById(R.id.txtHome);
+        mAdmin = (CheckBox) findViewById(R.id.chkAdmin);
+
+
+        Button mRegister = (Button) findViewById(R.id.btnSubmit);
+        mRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                attemptRegistration();
+            }
+        });
+
+        Button mCancel = (Button) findViewById(R.id.btnCancel);
+        mCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent loginPage = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(loginPage);
             }
         });
     }
 
+
+    /**
+     * Attempts to register the account specified by the registration form.
+     * If there are form errors (invalid email, missing fields, etc.), the
+     * errors are presented and no actual login attempt is made.
+     */
+    private void attemptRegistration() {
+        if (mAuthTask != null) {
+            return;
+        }
+
+        // Reset errors.
+        mUser.setError(null);
+        mPassword.setError(null);
+
+        // Store values at the time of the login attempt.
+        String email = mUser.getText().toString();
+        String password = mPassword.getText().toString();
+        String name = mName.getText().toString();
+        String home = mHome.getText().toString();
+        boolean isAdmin = mAdmin.isChecked();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        // Check for a valid password, if the user entered one.
+        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+            mPassword.setError(getString(R.string.error_invalid_password));
+            focusView = mPassword;
+            cancel = true;
+        }
+
+        // Check for a valid email address.
+        if (TextUtils.isEmpty(email)) {
+            mUser.setError(getString(R.string.error_field_required));
+            focusView = mUser;
+            cancel = true;
+        } else if (!isEmailValid(email)) {
+            mUser.setError(getString(R.string.error_invalid_email));
+            focusView = mUser;
+            cancel = true;
+        }
+
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView.requestFocus();
+        } else {
+            // kick off a background task to
+            // perform the user login attempt.
+            mAuthTask = new UserRegistrationTask(email, password, name, home, isAdmin);
+            mAuthTask.execute((Void) null);
+        }
+    }
+    private boolean isEmailValid(String email) {
+        //TODO: Replace this with your own logic
+        //return email.contains("@");
+        return email != null;
+    }
+
+    private boolean isPasswordValid(String password) {
+        //TODO: Replace this with your own logic
+        //return password.length() > 4;
+        return password != null;
+    }
+
+    /**
+     * Represents an asynchronous registration task used to authenticate
+     * the user.
+     */
+    private class UserRegistrationTask extends AsyncTask<Void, Void, Boolean> {
+
+        private final String mEmail;
+        private final String mPassword;
+        private final String mName;
+        private final String mHome;
+        private final boolean mAdmin;
+
+
+        UserRegistrationTask(String email, String password, String name, String home,
+                             boolean isAdmin) {
+            // TODO: implement password hashing -> the mPassword field should contain a hashed value
+            mEmail = email;
+            mPassword = password;
+            mName = name;
+            mHome = home;
+            mAdmin = isAdmin;
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            RatAppModel.checkInitialization();
+            RatAppModel model = RatAppModel.getInstance();
+
+            // TODO: check credentials, make sure all is valid
+
+            int regCode = model.registerUser(mEmail, mPassword, mName, mHome, mAdmin);
+
+            switch (regCode) {
+                case 0:
+                    // successful registration
+                    return true;
+                case 1:
+                    // Username taken
+                    return false;
+                case 2:
+                    // SQL error
+                    return false;
+                default:
+                    return false;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+            mAuthTask = null;
+
+            if (success) {
+                Log.i("RegistrationActivity", "onPostExecute Success");
+
+                Intent logoutScreen = new Intent(getApplicationContext(), LogoutActivity.class);
+                startActivity(logoutScreen);
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+            mAuthTask = null;
+        }
+    }
+
+    private class MakeDatabase extends AsyncTask<Void, Void, Boolean> {
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            RatAppModel.initialize();
+            return RatAppModel.getInstance().isDbInitialized();
+        }
+    }
 }
