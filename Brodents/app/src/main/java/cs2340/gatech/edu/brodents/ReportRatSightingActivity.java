a2ada407cb1f32e1880695f5b64f9b978680b822
Rikesh Subedi <rsubedi3@gatech.edu>
2017-10-17 22:19:32 -0400

    Implement rat report page UI and controller

--- Brodents/app/src/main/java/cs2340/gatech/edu/brodents/ReportRatSightingActivity.java
+++ Brodents/app/src/main/java/cs2340/gatech/edu/brodents/ReportRatSightingActivity.java
@@ -1,16 +1,120 @@
 package cs2340.gatech.edu.brodents;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Date;
 
 /**
  * Created by Benjamin Yarmowich on 10/15/2017.
+ * Implemented by Rikesh Subedi on 10/16/2017
  */
 
 public class ReportRatSightingActivity extends AppCompatActivity{
+
+    // UI Containers
+    EditText etIncidentAddress;
+    EditText etIncidentZIP;
+    EditText etCityName;
+    EditText etCityBorough;
+    EditText etLongitude;
+    EditText etLatitude;
+    EditText etComplaintType;
+    EditText etLocationType;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.report_sighting);
+
+        // UI Logic here
+        etIncidentAddress = (EditText) findViewById(R.id.etAddress);
+        etIncidentZIP = (EditText) findViewById(R.id.etZIP);
+        etCityName = (EditText) findViewById(R.id.etCityName);
+        etCityBorough = (EditText) findViewById(R.id.etCityBorough);
+        etLongitude = (EditText) findViewById(R.id.etLongitude);
+        etLatitude = (EditText) findViewById(R.id.etLatitude);
+        etComplaintType = (EditText) findViewById(R.id.etComplaintType);
+        etLocationType = (EditText) findViewById(R.id.etLocationType);
+
+        // TODO: make report button unclickable until all fields full
+
+
+        Button btnReport = (Button) findViewById(R.id.btnReport);
+        Button btnCancel = (Button) findViewById(R.id.btnCancel);
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+
+        btnReport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //attemptRegistration();
+                boolean success = submitReport();
+
+                if (success) {
+                    // Data has already been submitted in isValidSubmit() method
+                    finish();
+                }
+            }
+        });
+
+
+
     }
+
+
+    /**
+     * Function attempts to submit report and returns the success of the operation.
+     * @return the success of submitting the rat data
+     */
+    private boolean submitReport() {
+        String complaintType = etComplaintType.getText().toString();
+        String city = etCityName.getText().toString();
+        String borough = etCityBorough.getText().toString();
+        String address = etIncidentAddress.getText().toString();
+        String locationType = etLocationType.getText().toString();
+
+        // Problem children below
+        int incidentZIP = Integer.parseInt(etIncidentZIP.getText().toString());
+        double longitude = Double.parseDouble(etLongitude.getText().toString());
+        double latitude = Double.parseDouble(etLatitude.getText().toString());
+
+        if (complaintType.trim().length() == 0 || city.trim().length() == 0 ||
+                locationType.trim().length() == 0 || borough.trim().length() == 0 ||
+                address.trim().length() == 0 || incidentZIP == 0 || longitude == 0 ||
+                latitude == 0) {
+            // One of the fields is empty. Make specific if-statements later to specify message
+
+            // Display a toast warning the user that a field is incomplete
+            Context context = getApplicationContext();
+            CharSequence submitText = "Must complete form! Some fields are incomplete.";
+            int duration = Toast.LENGTH_LONG;
+
+            Toast.makeText(context, submitText, duration).show();
+            return false;
+        }
+
+
+        // TODO: check for valid data, ie. valid zip code
+
+
+        RatAppModel.getInstance().getSightingManager().insertSighting(complaintType, locationType,
+                incidentZIP, city, borough, address, latitude, longitude, null);
+
+        return true;
+    }
+
+
 }
