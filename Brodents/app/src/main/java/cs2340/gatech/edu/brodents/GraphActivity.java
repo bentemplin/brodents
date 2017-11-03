35b7ef8de3d7d9d7101a1a7328a33f1d6f45ab2a
bentemplin <bentemplin@users.noreply.github.com>
2017-11-03 19:10:02 -0400

    Get graph working

--- Brodents/app/src/main/java/cs2340/gatech/edu/brodents/GraphActivity.java
+++ Brodents/app/src/main/java/cs2340/gatech/edu/brodents/GraphActivity.java
@@ -10,8 +10,9 @@ import java.util.List;
 import java.util.Date;
 import java.sql.SQLException;
 import android.app.Activity;
-import android.content.DialogInterface;
+import android.content.res.Resources;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
@@ -21,7 +22,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.Description;
 import com.github.mikephil.charting.components.XAxis;
@@ -34,7 +34,10 @@ import com.github.mikephil.charting.utils.EntryXComparator;
 public class GraphActivity extends Activity {
     private LineChart mChart;
     private GraphDataFetcher dataFetcher;
-    private static final float DIVISOR = 10000;
+    private LineData data;
+    private Description title;
+    private static final float DIVISOR = 86400000; //This makes the change in x-axis one day
+    private static final float TEXT_SIZE = 18;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,9 +45,19 @@ public class GraphActivity extends Activity {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_graph);
             mChart = findViewById(R.id.graphLayout);
-            Description title = new Description();
+            title = new Description();
+            title.setText("Rat Sightings per Day");
+            title.setTextSize(TEXT_SIZE);
+            mChart.setDescription(title);
+            mChart.getLegend().setEnabled(false);
+            mChart.setScaleEnabled(true);
+            mChart.setDragEnabled(true);
             dataFetcher = null;
+            data = null;
             Button getGraph = findViewById(R.id.btnGetGraph);
+            title.setTextAlign(Paint.Align.CENTER);
+            int screenWidth = Resources.getSystem().getDisplayMetrics().widthPixels;
+            title.setPosition(screenWidth/2, mChart.getTop()+TEXT_SIZE*3);
             getGraph.setOnClickListener((View view) -> {
                 getDataGraph();
             });
@@ -80,18 +93,16 @@ public class GraphActivity extends Activity {
                 List<Entry> entries = new ArrayList<>();
                 long targetDay;
                 int count;
-                int rowCount = 0;
+                long firstDay = 0;
                 while (results.next()) {
                     targetDay = results.getTimestamp("DATE(createdDate)").getTime();
                     count = results.getInt("COUNT(*)");
-                    Log.i("GraphDataFetcher Day", Long.toString(targetDay));
-                    Log.i("GraphDataFetcher Count", Integer.toString(count));
-                    entries.add(new Entry((float) targetDay, count / DIVISOR));
-                    rowCount++;
+                    if (firstDay == 0) {
+                        firstDay = targetDay;
+                    }
+                    entries.add(new Entry(((targetDay-firstDay)/DIVISOR) , (float) count));
                 }
-                Log.i("GraphDataFetcher Size", Integer.toString(rowCount));
                 results.close();
-                Log.i("GraphDataFetcher", Integer.toString(entries.size()));
                 Collections.sort(entries, new EntryXComparator());
                 return new LineDataSet(entries, "Sightings v. Time");
             } catch (SQLException e) {
@@ -117,61 +128,41 @@ public class GraphActivity extends Activity {
         layout.addView(inputEnd);
         builder.setView(layout);
         //Set up buttons in the alert box
-        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                String inputTextStart = inputStart.getText().toString();
-                String inputTextEnd = inputEnd.getText().toString();
-                String datePattern = "MM-dd-yyyy";
-                SimpleDateFormat formatter = new SimpleDateFormat(datePattern);
-                try {
-                    Date startDateRaw = formatter.parse(inputTextStart);
-                    Date endDateRaw = formatter.parse(inputTextEnd);
-                    Date earliest = formatter.parse("01-01-2010");
-                    if (validateRange(startDateRaw, endDateRaw, earliest)) {
-                        dataFetcher = new GraphDataFetcher(startDateRaw, endDateRaw);
-                    } else {
-                        dataFetcher = null;
-                        return;
-                    }
-                } catch (ParseException e) {
-                    Log.e("InputClick", e.getMessage(), e);
-                    displayAlert("Invalid Date", "Input date in format MM-DD-YYYY");
+        builder.setPositiveButton("OK", (dialog, which) -> {
+            String inputTextStart = inputStart.getText().toString();
+            String inputTextEnd = inputEnd.getText().toString();
+            String datePattern = "MM-dd-yyyy";
+            SimpleDateFormat formatter = new SimpleDateFormat(datePattern);
+            try {
+                Date startDateRaw = formatter.parse(inputTextStart);
+                Date endDateRaw = formatter.parse(inputTextEnd);
+                Date earliest = formatter.parse("01-01-2010");
+                if (validateRange(startDateRaw, endDateRaw, earliest)) {
+                    dataFetcher = new GraphDataFetcher(startDateRaw, endDateRaw);
+                } else {
                     dataFetcher = null;
-                } catch (Exception e) {
-                    Log.e("InputClick", e.getMessage(), e);
-                    dataFetcher = null;
+                    return;
                 }
-                if (dataFetcher == null) {
-                    getDataGraph();
-                }
-                Description title = new Description();
-                mChart.setDescription(title);
-                mChart.setScaleEnabled(true);
-                mChart.setDragEnabled(true);
-                LineDataSet dataSet;
-                try {
-                    dataSet = dataFetcher.execute((Void) null).get();
-                    LineData data = new LineData(dataSet);
-                    data.setValueTextColor(Color.BLACK);
-                    mChart.setData(data);
-
-                    XAxis x1 = mChart.getXAxis();
-                    x1.setTextColor(Color.BLACK);
-                    x1.setDrawGridLines(false);
-                    x1.setAvoidFirstLastClipping(true);
-
-                    YAxis y1 = mChart.getAxisLeft();
-                    y1.setTextColor(Color.BLACK);
-                    y1.setDrawGridLines(true);
-                    y1.setAxisMaximum(100f);
-                    YAxis y2 = mChart.getAxisRight();
-                    //y2.setEnabled(false);
-                    mChart.invalidate();
-                    Log.i("getDataGraph", "At end of method!");
-                } catch (Exception e) {
-                    Log.e("GraphActivity", e.getMessage(), e);
-                }
+            } catch (ParseException e) {
+                Log.e("InputClick", e.getMessage(), e);
+                displayAlert("Invalid Date", "Input date in format MM-DD-YYYY");
+                dataFetcher = null;
+            } catch (Exception e) {
+                Log.e("InputClick", e.getMessage(), e);
+                dataFetcher = null;
+            }
+            if (dataFetcher == null) {
+                getDataGraph();
+            }
+            LineDataSet dataSet;
+            try {
+                dataSet = dataFetcher.execute((Void) null).get();
+                dataSet.setAxisDependency(YAxis.AxisDependency.LEFT);
+                data = new LineData(dataSet);
+                displayData();
+                Log.i("getDataGraph", "At end of method!");
+            } catch (Exception e) {
+                Log.e("GraphActivity", e.getMessage(), e);
             }
         });
         builder.show();
@@ -197,6 +188,32 @@ public class GraphActivity extends Activity {
         builder.setTitle(title);
         builder.setMessage(message);
         builder.show();
+    }
+
+    /**
+     * This method will display whatever is in the data field on the graph.
+     * To use, update the instance variable data and displayData will take care of the rest.
+     */
+    private void displayData() {
+        if (data != null) {
+            mChart.setData(data);
+            XAxis x1 = mChart.getXAxis();
+            x1.setTextColor(Color.BLACK);
+            x1.setDrawGridLines(false);
+            x1.setAvoidFirstLastClipping(true);
+            x1.setPosition(XAxis.XAxisPosition.BOTTOM);
+            x1.resetAxisMaximum();
+            x1.resetAxisMinimum();
+
+            YAxis y1 = mChart.getAxisLeft();
+            y1.setTextColor(Color.BLACK);
+            y1.setDrawGridLines(true);
+            y1.setAxisMinimum(0f);
+            YAxis y2 = mChart.getAxisRight();
+            y2.setEnabled(false);
+
+            mChart.invalidate();
+        }
     }
 }
 
